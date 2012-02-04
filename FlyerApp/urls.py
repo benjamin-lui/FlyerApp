65ce8e73599be987384cad49e452c7f8be28b121
Sahil Takiar <stakiar@berkeley.edu>
2012-02-04 00:06:17 -0800

    basic view for flyers/index

--- FlyerApp/urls.py
+++ FlyerApp/urls.py
@@ -5,13 +5,16 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'FlyerApp.views.home', name='home'),
-    # url(r'^FlyerApp/', include('FlyerApp.foo.urls')),
+	
+	url(r'^flyers/$', 'flyers.views.index'),
+	
+	# Examples:
+	# url(r'^$', 'FlyerApp.views.home', name='home'),
+	# url(r'^FlyerApp/', include('FlyerApp.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	# Uncomment the admin/doc line below to enable admin documentation:
+	# url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+	# Uncomment the next line to enable the admin:
+	# url(r'^admin/', include(admin.site.urls)),
 )
--- flyers/views.py
+++ flyers/views.py
@@ -1,1 +1,8 @@
 # Create your views here.
+
+from flyers.models import Flyer
+from django.http import HttpResponse
+
+def index(request):
+	
+	return HttpResponse("Hello, world. You're at the poll index.")
