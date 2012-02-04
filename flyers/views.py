2f2e2e383b040d83fce7c3db8b2b138643ae79fe
Benjamin Lui <benlui@berkeley.edu>
2012-02-04 00:50:16 -0800

    view stuff

--- flyers/views.py
+++ flyers/views.py
@@ -1,8 +1,11 @@
 # Create your views here.
 
+from django.template import Context, loader
 from flyers.models import Flyer
 from django.http import HttpResponse
 
 def index(request):
-	
-	return HttpResponse("Hello, world. You're at the poll index.")
+    latest_flyer_list = Flyer.objects.all()
+    t = loader.get_template('flyers/index.html')
+    c = Context({'latest_flyer_list': latest_flyer_list,})
+    return HttpResponse(t.render(c))
